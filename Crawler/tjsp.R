f56aa42860350719483c09bb9d3c67c51e56b329
Guilherme <gui.pereiracorrea@gmail.com>
2021-09-03 10:57:57 -0300

    script to separate big folders into smaller ones

--- Crawler/tjsp.R
+++ Crawler/tjsp.R
@@ -1,18 +1,63 @@
 devtools::install_github("jjesusfilho/tjsp")
 
 library(tjsp)
+######################## download the htmls ###############################
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "filhos", inicio="01/01/2018", fim="31/12/2018")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "filhos", inicio="01/01/2019", fim="31/12/2019")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "filhos", inicio="01/01/2020", fim="31/12/2020")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "filhos", inicio="01/01/2021", fim="31/08/2021")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "guarda", inicio="01/01/2018", fim="31/12/2018")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "guarda", inicio="01/01/2019", fim="31/12/2019")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "guarda", inicio="01/01/2020", fim="31/12/2020")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "guarda", inicio="01/01/2021", fim="31/08/2021")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "alienação parental", aspas=TRUE,  inicio="01/01/2018", fim="31/12/2018")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "alienação parental", aspas=TRUE,  inicio="01/01/2019", fim="31/12/2019")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "alienação parental", aspas=TRUE,  inicio="01/01/2020", fim="31/12/2020")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "alienação parental", aspas=TRUE,  inicio="01/01/2021", fim="31/08/2021")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "divórcio",  inicio="01/01/2018", fim="31/12/2018")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "divórcio",  inicio="01/01/2019", fim="31/12/2019")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "divórcio",  inicio="01/01/2020", fim="31/12/2020")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "divórcio",  inicio="01/01/2021", fim="31/08/2021")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "pensão",  inicio="01/01/2018", fim="31/12/2018")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "pensão",  inicio="01/01/2019", fim="31/12/2019")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "pensão",  inicio="01/01/2020", fim="31/12/2020")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "pensão",  inicio="01/01/2021", fim="31/08/2021")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "bens",  inicio="01/01/2018", fim="31/12/2018")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "bens",  inicio="01/01/2019", fim="31/12/2019")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "bens",  inicio="01/01/2020", fim="31/12/2020")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "bens",  inicio="01/01/2021", fim="31/08/2021")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "violência doméstica", aspas=TRUE,  inicio="01/01/2018", fim="31/12/2018")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "violência doméstica", aspas=TRUE,  inicio="01/01/2019", fim="31/12/2019")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "violência doméstica", aspas=TRUE,  inicio="01/01/2020", fim="31/12/2020")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "violência doméstica", aspas=TRUE,  inicio="01/01/2021", fim="31/08/2021")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "violência contra a mulher", aspas=TRUE,  inicio="01/01/2018", fim="31/12/2018")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "violência contra a mulher", aspas=TRUE,  inicio="01/01/2019", fim="31/12/2019")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "violência contra a mulher", aspas=TRUE,  inicio="01/01/2020", fim="31/12/2020")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "violência contra a mulher", aspas=TRUE,  inicio="01/01/2021", fim="31/08/2021")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "medida protetiva", aspas=TRUE,  inicio="01/01/2018", fim="31/12/2018")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "medida protetiva", aspas=TRUE,  inicio="01/01/2019", fim="31/12/2019")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "medida protetiva", aspas=TRUE,  inicio="01/01/2020", fim="31/12/2020")
+tjsp_baixar_cjpg(diretorio = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana", livre = "medida protetiva", aspas=TRUE,  inicio="01/01/2021", fim="31/08/2021")
 
-baixar_cjpg(diretorio = "/home/guilherme/Unicamp/IC_2021/Downloads", livre = "homofobia",inicio = "31/10/2018",fim = "30/10/2019")
-
-jpg_table <- ler_cjpg(diretorio = "/home/guilherme/Unicamp/IC_2021/Downloads")
-
+# After downloading the html pages the script sep_folders_script.sh should be run in order to avoid 
+# RAM overflow
+################################### Read the htmls into jsons ################################
 library(jsonlite)
 library(rvest)
-json_table_jpg <- toJSON(jpg_table,pretty = TRUE)
+# Read each subfolder
+parent_folder = "/home/guilherme/Unicamp/Veritas/Downloads_Fabiana/Fabiana"
+# List subfolders
+sub_folders = list.dirs(parent_folder)[-1]
+# Loop the subfolders htmls
+for (i in seq_along(sub_folders)){
+  print(sub_folders[i])
+  jpg_table <- ler_cjpg(diretorio = sub_folders[i])
+  json_table_jpg <- toJSON(jpg_table,pretty = TRUE)
+  jpg_file <- file(paste("/home/guilherme/Unicamp/Veritas/Downloads_Fabiana_json/jpg",i,".json",sep=""))
+  write(json_table_jpg,jpg_file)
+  close(jpg_file)
+}
 
-jpg_file <- file("/home/guilherme/Unicamp/IC_2021/Downloads/test_jpg.json")
-write(json_table_jpg,jpg_file)
-close(jpg_file)
 
 #################################################2º grau####################################################################################
 baixar_cjsg(diretorio = "/home/guilherme/Unicamp/IC_2021/Downloads", livre = "homofobia",inicio = "31/10/2018",fim = "30/10/2019")
